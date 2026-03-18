5b217d6b006cdbd99f1bd6dd7ad78112c8384042
RAHUL-al <parsadrahul832@gmail.com>
2026-03-18 08:59:49 +0530

    Update the code

--- create_files.sh
+++ create_files.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
-cd /home/ubuntu/
-source /home/ubuntu/stocks_env/bin/activate
-cd /home/ubuntu/backup_three_twenty_strategy
+cd /root/Trading_setup_code
+source /root/Trading_setup_code/venv/bin/activate
 
-nohup python create_angleone_csv.py >> create_angleone_csv.log 2>&1 &
-nohup python stocks_filter.py >> stocks_filter.log 2>&1 &
+nohup python3 create_angleone_csv.py >> create_angleone_csv.log 2>&1 &
+nohup python3 stocks_filter.py >> stocks_filter.log 2>&1 &
+
--- run_angleone.sh
+++ run_angleone.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-cd /home/ubuntu
-source /home/ubuntu/stocks_env/bin/activate
-cd /home/ubuntu/backup_three_twenty_strategy
+cd /root/Trading_setup_code
+source /root/Trading_setup_code/venv/bin/activate
 
 nohup python3 angleone_websocket1.py >> angleone1.log 2>&1 &
-nohup python3 angleone_websocket2.py >> angleone2.log 2>&1 &
-nohup python3 angleone_websocket3.py >> angleone3.log 2>&1 &
+
