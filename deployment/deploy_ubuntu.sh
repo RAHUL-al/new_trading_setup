82fc10ea099578513f31b76e8946997ccb1331c2
RAHUL-al <parsadrahul832@gmail.com>
2026-03-28 12:21:11 +0530

    Update the code for debug_setup.sh

--- deployment/deploy_ubuntu.sh
+++ deployment/deploy_ubuntu.sh
@@ -24,8 +24,8 @@ systemctl start fastapi
 
 echo "🌐 Setting up Nginx..."
 cp /root/Trading_setup_code/deployment/trading_setup.nginx /etc/nginx/sites-available/trading_setup
-# Replace IP inside Nginx config with the server's public IP automatically
-SERVER_IP=$(curl -s ifconfig.me)
+# Replace IP inside Nginx config with the server's public IPv4 automatically
+SERVER_IP=$(curl -s -4 ifconfig.me)
 sed -i "s/YOUR_DOMAIN_OR_SERVER_IP/$SERVER_IP/g" /etc/nginx/sites-available/trading_setup
 
 ln -s /etc/nginx/sites-available/trading_setup /etc/nginx/sites-enabled/
